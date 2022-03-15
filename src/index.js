68dc128a515006b9be67312a4f1f12b3fd5ad40e
TanveerNS <44171601+TanveerNS@users.noreply.github.com>
2022-03-15 22:18:39 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-export * from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import reportWebVitals from './reportWebVitals';
+import Routes from './config/Routes/index';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './index.css';
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <Routes />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
